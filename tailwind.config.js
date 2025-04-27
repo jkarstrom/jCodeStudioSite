d2e8b8d198b20f07170893b2c0020ce477b23aa5
jkarstrom <44649059+jkarstrom@users.noreply.github.com>
2025-04-27 12:09:07 -0500

    updates to config files for vercel

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,6 +17,33 @@ export default {
         green: '#cbe6c0',
         pink: '#eedbe6',
       },
+      animation: {
+        'bounce-slow': 'bounce 3s infinite',
+        'float': 'float 6s ease-in-out infinite',
+      },
+      keyframes: {
+        bounce: {
+          '0%, 100%': {
+            transform: 'translateY(-5%)',
+            animationTimingFunction: 'cubic-bezier(0.8,0,1,1)',
+          },
+          '50%': {
+            transform: 'translateY(0)',
+            animationTimingFunction: 'cubic-bezier(0,0,0.2,1)',
+          },
+        },
+        float: {
+          '0%': {
+            transform: 'translateY(0px)',
+          },
+          '50%': {
+            transform: 'translateY(-20px)',
+          },
+          '100%': {
+            transform: 'translateY(0px)',
+          },
+        },
+      },
     },
   },
   plugins: [],
