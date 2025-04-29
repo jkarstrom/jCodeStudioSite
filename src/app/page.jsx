a567c55c124db356961a202d22ff86e2ca930108
jkarstrom <44649059+jkarstrom@users.noreply.github.com>
2025-04-28 22:57:04 -0500

    Update page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-import Hero from "../components/page/hero";
-import AdventureCards from "../components/page/adventurecards";
-import TestimonialCard from "../components/page/testimonialcard";
+import Hero from "../components/page/Hero";
+import AdventureCards from "../components/page/AdventureCards";
+import TestimonialCard from "../components/page/TestimonialCard";
 
 export default function Home() {
   return (
@@ -22,7 +22,7 @@ export default function Home() {
         <TestimonialCard
           quote="Working with Joan has been an absolute pleasure! She transformed our product into a beautiful, functional site that we’re truly proud of. Her professionalism, sharp eye for detail, and deep commitment to creating the best possible product were evident in every phase of the project. Joan didn’t just approach this as a job, she embraced our organizational mission to make crime data more accessible to the Houston community and integrated that purpose into her work with care and dedication. She has been an invaluable asset to our team, and I’m genuinely looking forward to more opportunities to collaborate with her in the future."
           name="Nicki Wahlers"
-          title="Technical Program Manager"
+          title="Technical Program Manager "
           color="bg-pink"
         />
       </div>
